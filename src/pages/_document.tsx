b5972d1fc13661db03605cbf494c9fa7d931388a
Erick Oliveira <erickoliveirasystem@outlook.com>
2021-05-01 10:37:50 -0300

    favicon

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,7 +10,7 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Inter:wght@500&family=Lexend:wght@500;600&display=swap"
             rel="stylesheet"
           ></link>
-          <link rel="stylesheet" href="/favicon.png" type="image/png" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
         </Head>
         <body>
           <head>
